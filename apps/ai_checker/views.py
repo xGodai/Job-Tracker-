bf71c71f5e230422b6e97e81a85549e48f8a569b
GenericName192 <David-barton192@hotmail.co.uk>
2025-11-10 11:50:49 +0000

    added the function call_ai to handle calling the api when the cover letter is being checked.

--- apps/ai_checker/views.py
+++ apps/ai_checker/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from pypdf import PdfReader
+import os
+from azure.ai.inference import ChatCompletionsClient
+from azure.ai.inference.models import SystemMessage, UserMessage
+from azure.core.credentials import AzureKeyCredential
 
 
 @login_required
@@ -14,7 +19,37 @@ def cv_checker(request):
 @login_required
 def cover_letter_checker(request):
     if request.method == "POST":
-        # placeholder logic for AI
+        call_ai()
         pass
+
     if request.method == "GET":
         return render(request, "cover_letter_checker.html")
+
+
+def read_pdf(pdf):
+    reader = PdfReader(pdf)
+    pages = []
+    text = ""
+    for page in reader.pages:
+        pages.append(page)
+        text += page.extract_text()
+    return text
+
+
+def call_ai(cover_letter, job_description):
+    API_key = os.getenv("API_KEY")
+    API_address = "https://models.github.ai/inference"
+    model = "mistral-ai/mistral-small-2503"
+    client = ChatCompletionsClient(endpoint=API_address, credential=AzureKeyCredential(API_key),)
+
+    response = client.complete(
+        messages=[
+            SystemMessage("here is a cover letter and the provided job description, " +
+                          "give feedback on how to improve the cover letter and give it a score out of 10"),
+            UserMessage(f"cover letter: {cover_letter} \n job description: + {job_description}")],
+        temperature=0.8,
+        top_p=0.1,
+        max_tokens=2048,
+        model=model
+    )
+    return response
